a7d9af37ab90ed8b1eef2ea85e0e9d30fb0894cd
DavidHoerster <david@agileways.com>
2018-08-21 06:57:10 -0400

    initial project commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=baseball-bot-Web-Deploy.pubxml -p:Password=WuHwYSeXYhPw9BqdeSzfTwbCJoZChRnnArkQw0dqDatxsbD3QHeSNuPofoBy
+
