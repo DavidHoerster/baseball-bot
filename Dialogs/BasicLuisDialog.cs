bf73377bd80daa994ceb469281ddd2889cef820e
David Hoerster <david@agileways.com>
2018-08-22 07:48:47 -0400

    fixed some issues with connecting to MySQL

--- Dialogs/BasicLuisDialog.cs
+++ Dialogs/BasicLuisDialog.cs
@@ -54,7 +54,6 @@ namespace Microsoft.Bot.Sample.LuisBot
         [LuisIntent("PlayerStat")]
         public async Task GetPlayerStatIntent(IDialogContext context, LuisResult result)
         {
-            //await this.ShowLuisResult(context, result);
             int year = 2001;
             string first = "barry", last = "bonds", stat = "HR";
             int val;
@@ -77,7 +76,7 @@ namespace Microsoft.Bot.Sample.LuisBot
                     case "year":
                         year = Convert.ToInt32(ent.Entity);
                         break;
-                    case "name":
+                    case "player":
                         first = ent.Entity.Substring(0, ent.Entity.IndexOf(" "));
                         last = ent.Entity.Substring(ent.Entity.IndexOf(" ") + 1);
                         break;
@@ -91,31 +90,30 @@ namespace Microsoft.Bot.Sample.LuisBot
             var connStr = ConfigurationManager.ConnectionStrings["baseballData"].ConnectionString;
             #endregion
 
-            using (var conn = new MySqlConnection(connStr))
+            try
             {
-                conn.Open();
-                //HR value being hard-coded here...but can be parameterized
-                val = await conn.ExecuteScalarAsync<int>(@"select b.HR
-                                                            from master m
-	                                                            INNER JOIN batting b ON m.playerID = b.playerID
-                                                            where b.yearID = @year
-	                                                            AND m.nameLast = @last
-                                                                AND m.nameFirst = @first", new { @year = year, @first = first, @last = last });
+                using (var conn = new MySqlConnection(connStr))
+                {
+                    conn.Open();
+                    //HR value being hard-coded here...but can be parameterized
+                    val = await conn.ExecuteScalarAsync<int>(@"select b.HR
+                                                                from master m
+    	                                                            INNER JOIN batting b ON m.playerID = b.playerID
+                                                                where b.yearID = @year
+    	                                                            AND m.nameLast = @last
+                                                                    AND m.nameFirst = @first", new { @year = year, @first = first, @last = last });
 
+                }
+
+                await context.PostAsync($"{first} {last} hit {val} {stat} in {year}");
+                context.Wait(MessageReceived);
             }
-
-            await context.PostAsync($"{first} {last} hit {val} {stat} in {year}");
-            context.Wait(MessageReceived);
-
-            //var q = result.Query;
-            //StringBuilder sb = new StringBuilder();
-            //foreach (var ent in result.Entities)
-            //{
-            //    if (sb.Length != 0) { sb.Append(", "); }
-            //    sb.Append($"{ent.Type} = {ent.Entity}"); // = {q.Substring(ent.StartIndex.Value, ent.EndIndex.Value - ent.StartIndex.Value + 1)}");
-            //}
-            //await context.PostAsync(sb.ToString());
-            //context.Wait(MessageReceived);
+            catch (Exception exc)
+            {
+                await context.PostAsync($"Error occurred {exc.Message}");
+                context.Wait(MessageReceived);
+            }
+            
         }
 
 
